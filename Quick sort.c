8f12bd05151c78c2d028bd1dfe38763050e4985d
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-04-26 14:50:20 +0530

    Update and rename 5.c to Quick sort.c

--- Quick sort.c
+++ Quick sort.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
 int partition(int arr[],int low,int high)
 {
 	int pivot=arr[low];
